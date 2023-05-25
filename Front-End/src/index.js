81abbcf5be59e77b15f20f0531925b469faf9441
Kris Arndt <codewriting12@gmail.com>
2023-05-25 13:21:38 -0400

    Pushing with fixed import

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
