008a1d1f5d892533efe56ff87a7ddbd34f371fcb
Kris Arndt <codewriting12@gmail.com>
2023-05-25 13:20:29 -0400

    Pushing what I have

--- Front-End/src/API/axiosConfig.js
+++ Front-End/src/API/axiosConfig.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
 export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io/',
+    baseURL: 'https://www.freetogame.com/api/games',
     headers: {"ngrok-skip-browser-warning": true}
 })
---
+++ Front-End/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import api from './API/axiosConfig';
+import Layout from './components/Layout'
+import {Routes, Route } from 'react-router-dom'
+import Home from './components/home/Home';
+
+function App() {
+  const [games, setGames] = useState();
+
+  const getGames = async () => {
+    try {
+      const response = await api.get('/api/games');
+      console.log(response.data);
+      setGames(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  return (
+    <div className="App">
+
+      <Routes>
+         <Route path ="/" element= {<Layout/>}>
+          <Route path = "/" element = {<Home games ={games}/>}> </Route>
+
+         </Route>
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
+
