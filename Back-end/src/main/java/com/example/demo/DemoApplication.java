c84fe5d1e9159d64940239e081cf5522529f5958
unknown <ainouzusa@gmail.com>
2023-06-08 15:07:57 -0500

    security files

--- Back-end/src/main/java/com/example/demo/DemoApplication.java
+++ Back-end/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.demo")
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -18,3 +20,4 @@ public class DemoApplication {
     }
 
 }
+
